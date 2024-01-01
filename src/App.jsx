99b3c853f038c1cddd5faa58edcb069b6e9cfbcb
zahra <zhojabri@ymail.com>
2024-01-01 13:38:24 +0330

    add logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import "./App.css";
 import "./index.css";
 
 function App() {
-  return <div className="bg-rose-200 min-h-screen"></div>;
+  return (
+    <div className="bg-rose-200 min-h-screen">
+      <img src="/public/Logo.png" alt="logo" />
+    </div>
+  );
 }
 
 export default App;
