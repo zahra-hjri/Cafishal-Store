d85b65a777656e9f470799f69ee365b18ba4b7b2
zahra <zhojabri@ymail.com>
2024-01-02 21:17:32 +0330

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import "./index.css";
 import Logo from "./components/Logo/Logo";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="bg-rose-200 min-h-screen">
       <Logo />
+      <Navbar />
     </div>
   );
 }
