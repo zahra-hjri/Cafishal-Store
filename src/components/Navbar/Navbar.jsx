00ec1661ee49c22fe1b54df5039f54e84cb26641
zahra <zhojabri@ymail.com>
2024-01-03 20:42:11 +0330

    style navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <div className="bg-black">
-      <ul className="flex text-white">
+      <ul className="flex text-white font-bold p-2 gap-3 justify-center">
         <li>شال تابستانه</li>
         <li>شال گرم</li>
         <li>شومیز</li>
