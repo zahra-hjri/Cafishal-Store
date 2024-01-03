46d8e56af58b1b7f89f3122b2580492488cf5b62
zahra <zhojabri@ymail.com>
2024-01-03 20:40:22 +0330

    edit image and added navbar

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,7 +1,7 @@
 const Logo = () => {
   return (
     <div className="flex justify-center">
-      <img className="h-60 w-60" src="/Logo.png" alt="logo" />
+      <img className="" src="/Logo.png" alt="logo" />
     </div>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,15 @@
 const Navbar = () => {
-  return <div className="bg-black">Navbar</div>;
+  return (
+    <div className="bg-black">
+      <ul className="flex text-white">
+        <li>شال تابستانه</li>
+        <li>شال گرم</li>
+        <li>شومیز</li>
+        <li>کت</li>
+        <li>مانتو</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
