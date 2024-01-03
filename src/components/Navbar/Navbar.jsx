4a78b3dc15ace2d8eef18444296b57108715a5e6
zahra <zhojabri@ymail.com>
2024-01-03 20:43:50 +0330

    added style navbar fixed position

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="bg-black">
+    <div className="bg-black fixed top-2 w-full">
       <ul className="flex text-white font-bold p-2 gap-3 justify-center">
         <li>شال تابستانه</li>
         <li>شال گرم</li>
