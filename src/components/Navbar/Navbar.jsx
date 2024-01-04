b119ff947a51feb11d939515162c76e0069f644f
zahra <zhojabri@ymail.com>
2024-01-04 20:47:44 +0330

    added search icon and contact icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,20 @@
+import { IoMdContact } from "react-icons/io";
+import { IoSearchCircle } from "react-icons/io5";
+
 const Navbar = () => {
   return (
     <div className="bg-black fixed top-20 w-full">
-      <ul className="flex text-white font-bold p-2 justify-evenly">
+      <ul className="flex text-rose-200 p-4 justify-evenly">
+        <IoSearchCircle className="text-2xl" />
+        <IoMdContact className="text-2xl" />
         <li>شال تابستانه</li>
         <li>شال گرم</li>
         <li>شومیز</li>
         <li>کت</li>
         <li>مانتو</li>
       </ul>
+      {/* <div className="flex">
+      </div> */}
     </div>
   );
 };
