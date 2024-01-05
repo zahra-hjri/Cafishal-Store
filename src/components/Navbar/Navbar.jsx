a49f075a5294457bee2f0044a7fa829eb2fb413d
zahra <zhojabri@ymail.com>
2024-01-05 20:28:41 +0330

    styling mobile menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,19 +4,19 @@ import { IoMenu } from "react-icons/io5";
 
 const Navbar = ({ isOpenMobileMenu, onOpenMobileMenu }) => {
   return (
-    <div className="bg-black fixed top-20 w-full">
-      <ul className="flex text-rose-200 p-4 justify-evenly">
+    <div className="bg-black fixed top-20 w-2/3 md:w-full ">
+      <ul className="flex-col md:flex text-rose-200 p-4 justify-evenly">
         <IoSearchCircle className="text-2xl" />
         <IoMdContact className="text-2xl" />
         <div className="flex">
           {isOpenMobileMenu ? (
-            <>
+            <div className="">
               <li>شال تابستانه</li>
               <li>شال گرم</li>
               <li>شومیز</li>
               <li>کت</li>
               <li>مانتو</li>
-            </>
+            </div>
           ) : (
             <IoMenu onClick={onOpenMobileMenu} />
           )}
