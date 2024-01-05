967ea2ddc22abe1aab398bf9f2271be029a774d8
zahra <zhojabri@ymail.com>
2024-01-05 20:23:44 +0330

    handling mobile menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import "./App.css";
 import "./index.css";
 import Logo from "./components/Logo/Logo";
 import Navbar from "./components/Navbar/Navbar";
+import { useState } from "react";
 
 function App() {
+  const [isOpenMobileMenu, setIsOpenMobileMenu] = useState(false);
+  function handleOpenMobileMenu() {
+    setIsOpenMobileMenu((open) => !open);
+  }
   return (
     <div className="bg-rose-200 min-h-screen">
       <Logo />
-      <Navbar />
+      <Navbar
+        isOpenMobileMenu={isOpenMobileMenu}
+        onOpenMobileMenu={handleOpenMobileMenu}
+      />
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,26 @@
 import { IoMdContact } from "react-icons/io";
 import { IoSearchCircle } from "react-icons/io5";
+import { IoMenu } from "react-icons/io5";
 
-const Navbar = () => {
+const Navbar = ({ isOpenMobileMenu, onOpenMobileMenu }) => {
   return (
     <div className="bg-black fixed top-20 w-full">
       <ul className="flex text-rose-200 p-4 justify-evenly">
         <IoSearchCircle className="text-2xl" />
         <IoMdContact className="text-2xl" />
-        <li>شال تابستانه</li>
-        <li>شال گرم</li>
-        <li>شومیز</li>
-        <li>کت</li>
-        <li>مانتو</li>
+        <div className="flex">
+          {isOpenMobileMenu ? (
+            <>
+              <li>شال تابستانه</li>
+              <li>شال گرم</li>
+              <li>شومیز</li>
+              <li>کت</li>
+              <li>مانتو</li>
+            </>
+          ) : (
+            <IoMenu onClick={onOpenMobileMenu} />
+          )}
+        </div>
       </ul>
       {/* <div className="flex">
       </div> */}
