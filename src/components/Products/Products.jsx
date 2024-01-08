47a75c3e124ea75b416c2fa7dff3ebcaf1f67c97
zahra <zhojabri@ymail.com>
2024-01-08 19:44:34 +0330

    add fake api

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from "react";
 import Product from "../Product/Product";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setProducts(json));
+  }, []);
   return (
     <div className="">
-      <Product />
+      {products.map((item) => {
+        <Product key={item.id} />;
+      })}
     </div>
   );
 };
