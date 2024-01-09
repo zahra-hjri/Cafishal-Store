17dcf4b84ab647af1baec5ff1f19a43058317cd6
zahra <zhojabri@ymail.com>
2024-01-09 21:11:03 +0330

    added price and image to product from api

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,6 +2,8 @@ const Product = ({ product }) => {
   return (
     <div>
       <p>{product.title}</p>
+      <span>{product.price}</span>
+      <img src={product.image} className="h-20 w-20" />
     </div>
   );
 };
