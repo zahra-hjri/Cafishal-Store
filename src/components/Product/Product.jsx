bdab7184cf82360fad972cb924abc5b182b3804c
zahra <zhojabri@ymail.com>
2024-01-13 14:28:56 +0330

    styling image

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,7 +1,7 @@
 const Product = ({ product }) => {
   return (
-    <div className="w-80 h-80">
-      <img src={product.image} className="h-50 w-50" />
+    <div className="w-80 h-50 rounded-lg">
+      <img src={product.image} className="h-50 w-50 rounded-lg" />
       <p>{product.title}</p>
       <span>{product.price}</span>
     </div>
