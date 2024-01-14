53de45cc9cba7c64177df1d8b53d134852265d75
zahra <zhojabri@ymail.com>
2024-01-14 13:26:17 +0330

    styling products

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,6 +1,6 @@
 const Product = ({ product }) => {
   return (
-    <div className="w-96 h-96 rounded-lg bg-black">
+    <div className="w-72 h-96 rounded-lg bg-black">
       <img src={product.image} className="h-64 w-full rounded-lg" />
       <div className="flex justify-between">
         <p>{product.title}</p>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,7 +2,7 @@ import Product from "../Product/Product";
 
 const Products = ({ products }) => {
   return (
-    <div className=" flex justify-around">
+    <div className="p-8 flex justify-around">
       {products.map((item) => (
         // <p>{item.title}</p>
         <Product key={item.id} product={item} />
