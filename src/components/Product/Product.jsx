c9e1a0715ba86833e3815fa0d33fe0a105efe2c2
zahra <zhojabri@ymail.com>
2024-01-14 13:28:39 +0330

    styling cards product

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,7 +2,8 @@ const Product = ({ product }) => {
   return (
     <div className="w-72 h-96 rounded-lg bg-black">
       <img src={product.image} className="h-64 w-full rounded-lg" />
-      <div className="flex justify-around">
+      <div className="flex">
+        <span className="text-white">نام محصول:</span>
         <p className="font-mono font-bold text-white">{product.title}</p>
         <span className="font-mono font-bold text-white">{product.price}</span>
       </div>
