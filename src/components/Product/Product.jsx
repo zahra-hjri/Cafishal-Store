41c328fed0168da6f85d543d26733e37c6a3282d
zahra <zhojabri@ymail.com>
2024-01-14 13:25:23 +0330

    styling product

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,7 +1,7 @@
 const Product = ({ product }) => {
   return (
-    <div className="w-52 h-64 rounded-lg bg-black">
-      <img src={product.image} className="h-20 w-24 rounded-lg" />
+    <div className="w-96 h-96 rounded-lg bg-black">
+      <img src={product.image} className="h-64 w-full rounded-lg" />
       <div className="flex justify-between">
         <p>{product.title}</p>
         <span>{product.price}</span>
