6ac63f9acbf234baee7f2c7c94aac9a980263381
zahra <zhojabri@ymail.com>
2024-01-18 19:41:03 +0330

    styling footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="bg-gray-500 ">
+    <div className="bg-gray-500 p-5">
       <ul className="flex bg-slate-400">
         <li className="font-bold">shall</li>
         <li className="font-bold">scarf</li>
