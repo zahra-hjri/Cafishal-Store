fc8e979da1dae8559defdd415337e22416430b30
zahra <zhojabri@ymail.com>
2024-01-19 19:24:01 +0330

    adding styles in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,9 @@ import { GiAmpleDress } from "react-icons/gi";
 
 const Header = () => {
   return (
-    <div>
-      <GiAmpleDress />
-      <FaBars />
+    <div className="flex items-center justify-between p-5">
+      <GiAmpleDress className="text-3xl" />
+      <FaBars className="text-2xl" />
     </div>
   );
 };
