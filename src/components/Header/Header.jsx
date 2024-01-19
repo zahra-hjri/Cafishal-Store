a015a05b8d18eb468c8374b407a951df1d1f7fd8
zahra <zhojabri@ymail.com>
2024-01-19 19:21:41 +0330

    adding icons in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,13 @@
+import { FaBars } from "react-icons/fa";
+import { GiAmpleDress } from "react-icons/gi";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <GiAmpleDress />
+      <FaBars />
+    </div>
+  );
 };
 
 export default Header;
