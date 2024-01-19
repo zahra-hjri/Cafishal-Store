063fc08b489c83e8f35b2968d3b1dd99e5df03d8
zahra <zhojabri@ymail.com>
2024-01-19 19:11:46 +0330

    cleared all

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-// import { useState, useEffect } from "react";
 import "./App.css";
 import "./index.css";
-// import Logo from "./components/Logo/Logo";
-// import Navbar from "./components/Navbar/Navbar";
-// import Products from "./components/Products/Products";
-// import Footer from "./components/Footer/Footer";
 
 function App() {
-  // const [isOpenMobileMenu, setIsOpenMobileMenu] = useState(false);
-  // const [products, setProducts] = useState([]);
-  // function handleOpenMobileMenu() {
-  //   setIsOpenMobileMenu((open) => !open);
-  // }
-
-  // useEffect(() => {
-  //   const sendRequestData = async () => {
-  //     const response = await fetch("http://localhost:8000/products");
-  //     const responseData = await response.json();
-  //     setProducts(responseData);
-  //   };
-  //   sendRequestData();
-  // }, []);
   return <div className="bg-rose-200 min-h-screen"></div>;
 }
 
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,14 +1,1 @@
-import Product from "../Product/Product";
 
-const Products = ({ products }) => {
-  return (
-    <div className="p-8 flex justify-around">
-      {products.map((item) => (
-        // <p>{item.title}</p>
-        <Product key={item.id} product={item} />
-      ))}
-    </div>
-  );
-};
-
-export default Products;
