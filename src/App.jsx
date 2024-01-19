e3a8cda57d6be1c849fe1424e860f4b9c2595e5d
zahra <zhojabri@ymail.com>
2024-01-19 19:09:28 +0330

    cleared all

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,27 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import "./App.css";
 import "./index.css";
 // import Logo from "./components/Logo/Logo";
-import Navbar from "./components/Navbar/Navbar";
-import Products from "./components/Products/Products";
-import Footer from "./components/Footer/Footer";
+// import Navbar from "./components/Navbar/Navbar";
+// import Products from "./components/Products/Products";
+// import Footer from "./components/Footer/Footer";
 
 function App() {
-  const [isOpenMobileMenu, setIsOpenMobileMenu] = useState(false);
-  const [products, setProducts] = useState([]);
-  function handleOpenMobileMenu() {
-    setIsOpenMobileMenu((open) => !open);
-  }
+  // const [isOpenMobileMenu, setIsOpenMobileMenu] = useState(false);
+  // const [products, setProducts] = useState([]);
+  // function handleOpenMobileMenu() {
+  //   setIsOpenMobileMenu((open) => !open);
+  // }
 
-  useEffect(() => {
-    const sendRequestData = async () => {
-      const response = await fetch("http://localhost:8000/products");
-      const responseData = await response.json();
-      setProducts(responseData);
-    };
-    sendRequestData();
-  }, []);
-  return (
-    <div className="bg-rose-200 min-h-screen">
-      {/* <Logo /> */}
-      <Navbar
-        isOpenMobileMenu={isOpenMobileMenu}
-        onOpenMobileMenu={handleOpenMobileMenu}
-      />
-      <Products products={products} />
-      <Footer />
-    </div>
-  );
+  // useEffect(() => {
+  //   const sendRequestData = async () => {
+  //     const response = await fetch("http://localhost:8000/products");
+  //     const responseData = await response.json();
+  //     setProducts(responseData);
+  //   };
+  //   sendRequestData();
+  // }, []);
+  return <div className="bg-rose-200 min-h-screen"></div>;
 }
 
 export default App;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,5 @@
 const Footer = () => {
-  return (
-    <div className="bg-gray-500 p-5">
-      <ul className="flex bg-slate-400">
-        <li className="font-bold">shall</li>
-        <li className="font-bold">scarf</li>
-        <li className="font-bold">tshirt</li>
-      </ul>
-    </div>
-  );
+  return <div className=""></div>;
 };
 
 export default Footer;
