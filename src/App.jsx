99762c7287c72eee3c05649839e4623c591f6e8f
zahra <zhojabri@ymail.com>
2024-01-19 19:14:28 +0330

    adding Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import "./App.css";
 import "./index.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <div className="bg-rose-200 min-h-screen"></div>;
+  return (
+    <div className="bg-rose-200 min-h-screen">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Logo/Logo.jsx
+++
@@ -1,9 +0,0 @@
-const Logo = () => {
-  return (
-    <div className="flex justify-center ">
-      <img className="absolute -top-20" src="/Logo.png" alt="logo" />
-    </div>
-  );
-};
-
-export default Logo;
