364e4565eb755c8252259b114d80401fbfcc854a
zahra <zhojabri@ymail.com>
2024-01-19 19:36:43 +0330

    styling Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import "./index.css";
 import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="bg-rose-200 min-h-screen">
       <Header />
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,12 @@
-import { IoMdContact } from "react-icons/io";
-import { IoSearchCircle, IoMenu, IoCloseSharp } from "react-icons/io5";
-
-const Navbar = ({ isOpenMobileMenu, onOpenMobileMenu }) => {
+const Navbar = () => {
   return (
-    <div className="bg-black  w-full ">
-      <ul className="flex text-rose-200 p-4 justify-evenly">
-        {/* <img className="absolute -top-20" src="/Logo.png" alt="logo" /> */}
-        <div className="flex gap-2">
-          <IoSearchCircle className="text-2xl" />
-          <IoMdContact className="text-2xl" />
-        </div>
-        <div className="flex">
-          {isOpenMobileMenu ? (
-            <div className="flex justify-between w-80">
-              <IoCloseSharp className="text-2xl" onClick={onOpenMobileMenu} />
-              <div className="">
-                <li>شال تابستانه</li>
-                <li>شال گرم</li>
-                <li>شومیز</li>
-                <li>کت</li>
-                <li>مانتو</li>
-              </div>
-            </div>
-          ) : (
-            <IoMenu className="text-2xl" onClick={onOpenMobileMenu} />
-          )}
-        </div>
+    <div className="bg-slate-950 w-2/3 text-rose-200 h-full">
+      <ul>
+        <li>خانه</li>
+        <li>درباره ما</li>
+        <li>تماس با ما</li>
+        <li>رضایت </li>
       </ul>
-      {/* <div className="flex">
-      </div> */}
     </div>
   );
 };
