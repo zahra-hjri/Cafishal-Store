8837e6c6ba5782970f95918c0fe01eaceda82d00
zahra <zhojabri@ymail.com>
2024-01-20 21:09:51 +0330

    styling navbar item

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,11 +3,11 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <div className="bg-slate-950 w-2/3 text-rose-200 h-full">
-      <ul className="text-xs">
-        <li className="py-2 cursor-pointer">خانه</li>
-        <li className="py-2 cursor-pointer">درباره ما</li>
-        <li className="py-2 cursor-pointer">تماس با ما</li>
-        <li className="py-2 cursor-pointer">رضایت </li>
+      <ul className="text-xs p-5">
+        <li className="py-1 cursor-pointer">خانه</li>
+        <li className="py-1 cursor-pointer">درباره ما</li>
+        <li className="py-1 cursor-pointer">تماس با ما</li>
+        <li className="py-1 cursor-pointer">رضایت </li>
       </ul>
     </div>
   );
