00b6408d221589c9271d707276b410870d2f6638
zahra <zhojabri@ymail.com>
2024-01-23 10:22:14 +0330

    added a tag to nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,12 +4,24 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-950 w-2/3 md:w-1/3 text-rose-200 h-screen fixed z-10">
       <ul className="text-xs p-5">
-        <li className="py-1 cursor-pointer">خانه</li>
-        <li className="py-1 cursor-pointer">درباره ما</li>
-        <li className="py-1 cursor-pointer">تماس با ما</li>
-        <li className="py-1 cursor-pointer">رضایت </li>
-        <li className="py-1 cursor-pointer">توجه </li>
-        <li className="py-1 cursor-pointer">برندینگ </li>
+        <li className="py-1">
+          <a href="">خانه</a>
+        </li>
+        <li className="py-1">
+          <a href="">درباره ما</a>
+        </li>
+        <li className="py-1">
+          <a href="">تماس با ما</a>
+        </li>
+        <li className="py-1">
+          <a href="">رضایت</a>
+        </li>
+        <li className="py-1">
+          <a href="">توجه</a>
+        </li>
+        <li className="py-1">
+          <a href="">برندینگ</a>
+        </li>
       </ul>
     </nav>
   );
