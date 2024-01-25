f112d202e48f21d0350b8aaa1b769efe5bd75586
Zahra Hozhabri <90372342+zahra-hjri@users.noreply.github.com>
2024-01-25 22:56:35 +0330

    Update Navbar.jsx add new class

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
         <li className="py-1">
           <a href="">برندینگ</a>
         </li>
-        <li>
+        <li className="py-1">
           <a href="">تخفیف</a>
         </li>
       </ul>
