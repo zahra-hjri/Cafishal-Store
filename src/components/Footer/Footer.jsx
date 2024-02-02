6d386631d5593f403567b40a5da540068c024a4f
Zahra Hozhabri <90372342+zahra-hjri@users.noreply.github.com>
2024-02-02 08:42:26 +0330

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 const Footer = () => {
+  const [id,setId]=UseState("")
   return <div className="flex text-gray-950 my-1 p-4 justify-center items-center">
   <li>instagram</li>
     <li>telegram</li>
