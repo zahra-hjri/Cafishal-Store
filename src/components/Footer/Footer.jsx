0d2493685d428be47c020a8d9b5dc2e57be15c07
Zahra Hozhabri <90372342+zahra-hjri@users.noreply.github.com>
2024-02-04 10:15:47 +0330

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return <div className="flex text-gray-950 my-1 p-3 justify-center items-center text-xs">
   <li className="px-1">instagram</li>
     <li className="px-1">telegram</li>
-    <li>what's app</li>
+    <li className="px-1">what's app</li>
     <li>Twitter</li>
     <li>zoom</li>
   </div>;
