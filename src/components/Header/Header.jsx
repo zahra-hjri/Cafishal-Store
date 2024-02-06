de32d390f9d5018a053f77118aae7dcf3c459add
Zahra Hozhabri <90372342+zahra-hjri@users.noreply.github.com>
2024-02-05 23:06:25 -1000

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { GiAmpleDress } from "react-icons/gi";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between p-5 text-gray-950">
+    <header className="flex items-center justify-between p-5 text-gray-900">
       <a href="/">
         <GiAmpleDress className="text-4xl md:text-5xl" />
       </a>
