49548e22faef49ede829407fca1f98e5f698688a
Zahra Hozhabri <90372342+zahra-hjri@users.noreply.github.com>
2024-02-08 23:27:30 +0330

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <li className="px-1">telegram</li>
     <li className="px-1">what's app</li>
     <li className="px-1">Twitter</li>
-    <li>zoom</li>
+    <li className="px-1">zoom</li>
   </div>;
 };
 
