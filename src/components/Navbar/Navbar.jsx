0affa5213c6451a5d827f213b476bca82a1e88f1
Zahra Hozhabri <90372342+zahra-hjri@users.noreply.github.com>
2024-02-25 11:35:44 +0330

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-950 w-2/3 md:w-1/3 text-rose-400 h-screen fixed z-20">
+    <nav className="bg-gray-950 w-2/3 md:w-1/3 text-rose-300 h-screen fixed z-20">
       <ul className="text-xs p-5 flex justify-between">
         <li className="py-2">
           خانه
