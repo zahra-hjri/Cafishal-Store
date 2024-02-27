d6469c4ec73d60be4d27b4497c93afb00a07c92a
Zahra Hozhabri <90372342+zahra-hjri@users.noreply.github.com>
2024-02-27 23:45:38 +0330

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const handled = () =>{
     console.log("zarin")
   }
-  return <div className="flex text-white my-4 p-5 justify-center items-center text-md">
+  return <div className="flex text-white my-4 p-5 justify-center items-center text-lg">
   <li className="px-1 text-sm md:text-sm lg:text-md px-2">instam</li>
     <li className="px-1 text-sm md:text-sm lg:text-md px-2">telegram</li>
     <li className="px-1 text-sm md:text-sm lg:text-md px-2">what's app</li>
