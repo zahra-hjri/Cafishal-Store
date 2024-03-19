686d2fb9377b7c3c46552d286a1c341871c3f177
Zahra Hozhabri <90372342+zahra-hjri@users.noreply.github.com>
2024-03-18 20:41:46 -1100

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <li className="py-1 text-white text-xs">
           تماس با ما
         </li>
-        <li className="py-1 text-white">
+        <li className="py-1 text-white text-xs">
           <a href="">رضایت</a>
         </li>
         <li className="py-1 text-white">
