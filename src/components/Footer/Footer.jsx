b673732515dce79bd3a8ad8350d876fbb5130eb1
Zahra Hozhabri <90372342+zahra-hjri@users.noreply.github.com>
2024-03-27 19:36:14 -1100

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   <li className="text-sm md:text-sm lg:text-md p-3 text-gray-300">insta</li>
     <li className="text-sm md:text-sm lg:text-md p-3 text-gray-300">telegram</li>
     <li className="text-sm md:text-sm lg:text-md p-3 text-gray-300">what's app</li>
-    <li className="text-sm md:text-sm lg:text-md p-2 text-gray-300">Twitter</li>
+    <li className="text-sm md:text-sm lg:text-md p-3 text-gray-300">Twitter</li>
     <li className="text-sm md:text-sm lg:text-md p-2 text-gray-300">discord</li>
   </div>;
 };
