3c71f9b8d7fb54d714c3d7e2d97df740f75aaafb
Zahra Hozhabri <90372342+zahra-hjri@users.noreply.github.com>
2024-04-25 23:40:39 -1100

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
           <li className="py-2 text-white text-xs">
             تماس با ما
           </li>
-          <li className="py-2 text-xs">
+          <li className="py-2 text-xs text-white">
             رضایت
           </li>
           <li className="py-2 text-xs">
