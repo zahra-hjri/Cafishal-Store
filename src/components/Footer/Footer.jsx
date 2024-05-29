fae3a15e9c824f549cec16e8bb2c7df1ec00b779
Zahra Hozhabri <90372342+zahra-hjri@users.noreply.github.com>
2024-05-29 19:53:12 +0330

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return <div className="flex text-white mt-3 justify-between items-center">
   <li className="text-sm md:text-md lg:text-md p-3 text-gray-400 mt-2">insta</li>
     <li className="text-sm md:text-md lg:text-md p-2 text-gray-400 mt-2">telegram</li>
-    <li className="text-sm md:text-md lg:text-md p-2 text-gray-200 mt-2">what's app</li>
+    <li className="text-sm md:text-md lg:text-md p-2 text-gray-400 mt-2">what's app</li>
     <li className="text-sm md:text-md lg:text-md p-2 text-gray-200 mt-2">Twitter</li>
     <li className="text-sm md:text-md lg:text-md p-2 text-gray-200 mt-2">discord</li>
     <li className="text-sm md:text-md lg:text-md p-2 text-gray-300 mt-2"></li>
