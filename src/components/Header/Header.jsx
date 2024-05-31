ddeebd23a4b796c9abb319ba7b77e422a514710e
Zahra Hozhabri <90372342+zahra-hjri@users.noreply.github.com>
2024-05-31 22:05:01 +0330

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <GiAmpleDress className="text-lg md:text-xl mx-5" />
       </a>
       <nav>
-        <ul className="text-sm md:text-md lg:text-lg p-4 flex gap-6 font-normal">
+        <ul className="text-sm md:text-md lg:text-lg p-3 flex gap-6 font-normal">
           <li className="py-2 text-white text-xs">خانه</li>
           <li className="py-2 text-white text-xs">درباره ما</li>
           <li className="py-2 text-white text-xs">
