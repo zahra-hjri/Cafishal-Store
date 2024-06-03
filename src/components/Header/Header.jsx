7f16e61840fb950f0ab07cf37624d22b6d4d3a45
Zahra Hozhabri <90372342+zahra-hjri@users.noreply.github.com>
2024-06-03 02:49:44 -1100

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       </a>
       <nav>
         <ul className="text-sm md:text-md lg:text-lg p-3 flex gap-6 font-normal">
-          <li className="py-2 text-white text-xs">خانه</li>
+          <li className="py-2 text-white text-xs font-bold">خانه</li>
           <li className="py-2 text-white text-xs">درباره ما</li>
           <li className="py-2 text-white text-xs">
             تماس با ما
