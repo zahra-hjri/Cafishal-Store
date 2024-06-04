5d91e9edf88c6ccb744aaa4aac9015f8d5a53cb0
zahra <zhojabri@ymail.com>
2024-06-04 09:47:14 +0330

    login redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
+// import { Provider } from "react-redux";
 import "./App.css";
 import "./index.css";
 // import Header from "./components/Header/Header";
 import Login from "./components/Login/Login";
-import { Provider } from "react-redux";
+// import store from "/src/Store.js";
 
 // import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
+    // <Provider>
     <div className="bg-gray-900 w-full h-screen">
       {/* <Header /> */}
       {/* <Navbar /> */}
-      <Provider>
-        <Login />
-      </Provider>
+
+      <Login />
     </div>
+    // </Provider>
   );
 };
 
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,40 @@
 // import React from 'react'
 
+import { useState } from "react";
+import { login, logout } from "../../Store";
+import { useDispatch, useSelector } from "react-redux";
+
 const Login = () => {
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+  const selector = useSelector((state) => state.user);
   return (
     <div className="w-1/2 mx-auto pt-10 ">
-      <h3 className="text-green-500 font-bold">login</h3>
+      <h3 className="text-green-500 font-bold">
+        login
+        <span className="text-pink-500 font-bold px-3">
+          {selector.username}
+        </span>
+      </h3>
       <input
+        onChange={(e) => setInput(e.target.value)}
         className="w-80 rounded-sm h-[40px] mt-2 px-3"
         placeholder="username"
       />
+      <div className="flex gap-5 mt-3">
+        <button
+          onClick={() => dispatch(login({ username: input }))}
+          className="bg-green-600 text-white rounded-sm p-1"
+        >
+          login
+        </button>
+        <button
+          onClick={() => dispatch(logout())}
+          className="bg-red-600 text-white rounded-sm p-1"
+        >
+          logout
+        </button>
+      </div>
     </div>
   );
 };
