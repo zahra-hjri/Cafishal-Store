ec4a7eef5136a5e5474824b5aa91f73f865dcf3a
zahra <zhojabri@ymail.com>
2024-08-03 18:10:36 +0330

    promise

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const App = () => {
+  const initialStories = [
+    { id: 1, name: "React", points: 52 },
+    { id: 2, name: "Redux", points: 21 },
+    { id: 3, name: "Java", points: 14 },
+  ];
+
+  const [stories, setStories] = useState(initialStories);
+  const getAsynchronous = () => {
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ data: { stories: initialStories } });
+      }, 2000);
+    });
+  };
+  // useEffect(() => {
+  //   getAsynchronous().then((result) => {
+  //     setStories(result.data.stories);
+  //   });
+  // }, []);
   return (
     <div className="bg-gray-900 h-screen text-white font-semibold">
-      Hello react
+      {stories.map((item) => {
+        <div>{item.id}</div>;
+      })}
     </div>
   );
 };
